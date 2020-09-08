c993bb1525ba7eee6a3cfba40a26ddff86b7677e
Agnieszka Szczepanska <aga.szczepanska@hotmail.com>
2020-09-08 09:18:23 +0100

    fix collecting signarutes

--- workflows/src/main/kotlin/com/template/flows/example/simpleState/UpdateSimpleStateForAccount.kt
+++ workflows/src/main/kotlin/com/template/flows/example/simpleState/UpdateSimpleStateForAccount.kt
@@ -5,9 +5,11 @@ import com.r3.corda.lib.tokens.workflows.utilities.getPreferredNotary
 import com.template.contracts.example.SimpleStateContract
 import com.template.states.example.SimpleState
 import net.corda.core.contracts.StateAndRef
+import net.corda.core.contracts.requireThat
 import net.corda.core.flows.*
 import net.corda.core.identity.AbstractParty
 import net.corda.core.node.StatesToRecord
+import net.corda.core.transactions.SignedTransaction
 import net.corda.core.transactions.TransactionBuilder
 
 @InitiatingFlow
@@ -56,14 +58,20 @@ class UpdateSimpleStateForAccount(
 
 
 @InitiatedBy(UpdateSimpleStateForAccount::class)
-class UUpdateSimpleStateForAccountResponder(
+class UpdateSimpleStateForAccountResponder(
     private val otherSession: FlowSession
 ) : FlowLogic<Unit>() {
     @Suspendable
     override fun call() {
+        val signTransactionFlow = object : SignTransactionFlow(otherSession) {
+            override fun checkTransaction(stx: SignedTransaction) = requireThat {
+            }
+        }
+        val transaction = subFlow(signTransactionFlow)
         subFlow(
             ReceiveFinalityFlow(
                 otherSession,
+                expectedTxId = transaction.id,
                 statesToRecord = StatesToRecord.ALL_VISIBLE
             )
         )
