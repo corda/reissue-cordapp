840d5031e7374ff2a70395828fb4b7138e7b379f
Agnieszka Szczepanska <aga.szczepanska@hotmail.com>
2020-09-14 13:41:09 +0100

    UnlockReIssuedStateTest refactoring

--- contracts/src/test/kotlin/com/template/UnlockReIssuedStateTest.kt
+++ contracts/src/test/kotlin/com/template/UnlockReIssuedStateTest.kt
@@ -2,6 +2,7 @@ package com.template
 
 import com.template.contracts.ReIssuanceLockContract
 import com.template.contracts.example.SimpleStateContract
+import com.template.states.example.SimpleState
 import net.corda.core.contracts.StateRef
 import net.corda.core.crypto.Crypto
 import net.corda.core.crypto.SignableData
@@ -19,29 +20,14 @@ class UnlockReIssuedStateTest: AbstractContractTest() {
         val reIssuedStateLabel = "re-issued state encumbered by re-issuance lock"
         val dummyState = createDummyState()
 
+        val deleteDummyStateTransaction = generateDeleteDummyStateTransaction(dummyState)
+        val deletedTransactionInputStream = generateSignedTransactionByteArray(deleteDummyStateTransaction).inputStream()
+
+        val uploadedDeletedTransactionSecureHash = aliceNode.services.attachments.importAttachment(
+            deletedTransactionInputStream, aliceParty.toString(), null)
+        val deletedStateRef: StateRef = deleteDummyStateTransaction.coreTransaction.inputs[0]
+
         aliceNode.services.ledger(notary = notaryParty) {
-            val deleteStateTransaction: WireTransaction = unverifiedTransaction {
-                input(SimpleStateContract.contractId, dummyState)
-                command(listOf(aliceParty.owningKey), SimpleStateContract.Commands.Delete())
-            }
-
-            val requesterSignatureMetadata = SignatureMetadata(aliceNode.services.myInfo.platformVersion,
-                Crypto.findSignatureScheme(aliceParty.owningKey).schemeNumberID)
-            val requesterSignableData = SignableData(deleteStateTransaction.id, requesterSignatureMetadata)
-            val requesterSig = aliceNode.services.keyManagementService.sign(requesterSignableData, aliceParty.owningKey)
-
-            val notarySignatureMetadata = SignatureMetadata(notaryNode.services.myInfo.platformVersion,
-                Crypto.findSignatureScheme(notaryParty.owningKey).schemeNumberID)
-            val notarySignableData = SignableData(deleteStateTransaction.id, notarySignatureMetadata)
-            val notarySig = notaryNode.services.keyManagementService.sign(notarySignableData, notaryParty.owningKey)
-
-            val deleteDummyStateTransaction = SignedTransaction(deleteStateTransaction, listOf(requesterSig, notarySig))
-            val deletedTransactionInputStream = generateSignedTransactionByteArray(deleteDummyStateTransaction).inputStream()
-
-            val uploadedDeletedTransactionSecureHash = aliceNode.services.attachments.importAttachment(
-                deletedTransactionInputStream, aliceParty.toString(), null)
-            val deletedStateRef: StateRef = deleteStateTransaction.inputs[0]
-
             unverifiedTransaction {
                 output(ReIssuanceLockContract.contractId, reIssuanceLockLabel,
                     contractState=createDummyReIssuanceLock(stateRef = deletedStateRef), encumbrance = 1)
@@ -59,5 +45,29 @@ class UnlockReIssuedStateTest: AbstractContractTest() {
                 verifies()
             }
         }
+    }
+
+    fun generateDeleteDummyStateTransaction(dummyState: SimpleState): SignedTransaction {
+        var nullableDeleteStateTransaction: WireTransaction? = null
+        aliceNode.services.ledger(notary = notaryParty) {
+            nullableDeleteStateTransaction = unverifiedTransaction {
+                input(SimpleStateContract.contractId, dummyState)
+                command(listOf(aliceParty.owningKey), SimpleStateContract.Commands.Delete())
+            }
+        }
+        require(nullableDeleteStateTransaction != null) { "deleteStateTransaction hasn't been initialized" }
+        val deleteStateTransaction = nullableDeleteStateTransaction!!
+
+        val requesterSignatureMetadata = SignatureMetadata(aliceNode.services.myInfo.platformVersion,
+            Crypto.findSignatureScheme(aliceParty.owningKey).schemeNumberID)
+        val requesterSignableData = SignableData(deleteStateTransaction.id, requesterSignatureMetadata)
+        val requesterSig = aliceNode.services.keyManagementService.sign(requesterSignableData, aliceParty.owningKey)
+
+        val notarySignatureMetadata = SignatureMetadata(notaryNode.services.myInfo.platformVersion,
+            Crypto.findSignatureScheme(notaryParty.owningKey).schemeNumberID)
+        val notarySignableData = SignableData(deleteStateTransaction.id, notarySignatureMetadata)
+        val notarySig = notaryNode.services.keyManagementService.sign(notarySignableData, notaryParty.owningKey)
+
+        return SignedTransaction(deleteStateTransaction, listOf(requesterSig, notarySig))
     }
 }
