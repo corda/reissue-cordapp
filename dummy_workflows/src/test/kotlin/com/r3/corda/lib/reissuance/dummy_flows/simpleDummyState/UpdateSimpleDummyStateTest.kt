96dc049b86065546018226585097ebc56b185b20
Agnieszka Szczepanska <aga.szczepanska@hotmail.com>
2020-10-08 19:40:31 +0100

    dummy_workflows module

---
+++ dummy_workflows/src/test/kotlin/com/r3/corda/lib/reissuance/dummy_flows/simpleDummyState/UpdateSimpleDummyStateTest.kt
@@ -0,0 +1,64 @@
+package com.r3.corda.lib.reissuance.dummy_flows.simpleDummyState
+
+import com.r3.corda.lib.reissuance.dummy_flows.AbstractFlowTest
+import com.r3.corda.lib.reissuance.dummy_states.SimpleDummyState
+import net.corda.core.identity.AbstractParty
+import org.hamcrest.MatcherAssert.*
+import org.hamcrest.Matchers.*
+import org.junit.Test
+
+class UpdateSimpleDummyStateTest: AbstractFlowTest() {
+
+    @Test
+    fun `Update SimpleDummyState`() {
+        initialiseParties()
+        createSimpleDummyState(aliceParty)
+        updateSimpleDummyState(aliceNode, bobParty)
+
+        val simpleDummyStates = getStateAndRefs<SimpleDummyState>(bobNode)
+        assertThat(simpleDummyStates, hasSize(1))
+        val simpleDummyState = simpleDummyStates[0].state.data
+        assertThat(simpleDummyState.owner, `is`(bobParty as AbstractParty))
+    }
+
+    @Test
+    fun `Update SimpleDummyState many times`() {
+        initialiseParties()
+        createSimpleDummyState(aliceParty)
+        updateSimpleDummyState(aliceNode, bobParty)
+        updateSimpleDummyState(bobNode, charlieParty)
+        updateSimpleDummyState(charlieNode, debbieParty)
+        updateSimpleDummyState(debbieNode, charlieParty)
+        updateSimpleDummyState(charlieNode, bobParty)
+        updateSimpleDummyState(bobNode, aliceParty)
+
+        val simpleDummyStates = getStateAndRefs<SimpleDummyState>(aliceNode)
+        assertThat(simpleDummyStates, hasSize(1))
+        val simpleDummyState = simpleDummyStates[0].state.data
+        assertThat(simpleDummyState.owner, `is`(aliceParty as AbstractParty))
+    }
+
+    @Test
+    fun `Update SimpleDummyState - accounts on the same host`() {
+        initialisePartiesForAccountsOnTheSameHost()
+        createSimpleDummyStateForAccount(employeeNode, employeeAliceParty)
+        updateSimpleDummyStateForAccount(employeeNode, employeeBobParty)
+
+        val simpleDummyStates = getStateAndRefs<SimpleDummyState>(employeeNode)
+        assertThat(simpleDummyStates, hasSize(1))
+        val simpleDummyState = simpleDummyStates[0].state.data
+        assertThat(simpleDummyState.owner, `is`(employeeBobParty))
+    }
+
+    @Test
+    fun `Update SimpleDummyState - accounts on different hosts`() {
+        initialisePartiesForAccountsOnDifferentHosts()
+        createSimpleDummyStateForAccount(issuerNode, employeeAliceParty)
+        updateSimpleDummyStateForAccount(aliceNode, employeeBobParty)
+
+        val simpleDummyStates = getStateAndRefs<SimpleDummyState>(bobNode)
+        assertThat(simpleDummyStates, hasSize(1))
+        val simpleDummyState = simpleDummyStates[0].state.data
+        assertThat(simpleDummyState.owner, `is`(employeeBobParty))
+    }
+}
